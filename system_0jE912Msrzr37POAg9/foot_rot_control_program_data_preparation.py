4753a0f32666df9680576c924300175b77ecf1f4
cype <martin@cube7.ch>
2025-08-05 15:48:57 +0200

    update

--- system_0jE912Msrzr37POAg9/foot_rot_control_program_data_preparation.py
+++ system_0jE912Msrzr37POAg9/foot_rot_control_program_data_preparation.py
@@ -6,36 +6,29 @@
 #df.to_csv("./ogd/foot_rot_control_program/Cubes/moderhinke-cube.csv", sep=";")
 #df.to_csv("./ogd/foot_rot_control_program/eradication-foot-rot.csv", index=False)
 
-
 import os
-import re
 from ftplib import FTP
-import pandas as pd
 
 # GitHub secret
 FTP_PASS = os.environ["FTP_PASS_DATEN_ALVPH"]
 
-# Local directory to save downloaded files
+# Lokaler Speicherort für heruntergeladene Datei
 local_directory = "temporary_files"
+filename = "moderhinke-aktuelle-situation-input.csv"
+local_path = os.path.join(local_directory, filename)
 
-# Connect to the FTP server
-ftp = FTP('ftp.blv-data-ingest.ch')
-ftp.login(user='Daten_ALVPH@blv-data-ingest.ch', passwd=FTP_PASS)
-ftp.cwd('Moderhinke_2024_2025')
-
-# Get a list of files on the server
-files = []
-ftp.retrlines('LIST', files.append)
-
-ftp.quit()
-
-# Speicherort der Textdatei
-output_path = os.path.join(local_directory, "ftp_file_list.txt")
-
-# Stelle sicher, dass das Verzeichnis existiert
+# Sicherstellen, dass das lokale Verzeichnis existiert
 os.makedirs(local_directory, exist_ok=True)
 
-# Inhalt in Datei schreiben
-with open(output_path, "w", encoding="utf-8") as f:
-    for line in files:
-        f.write(line + "\n")
+# Verbindung zum FTP-Server
+ftp = FTP('ftp.blv-data-ingest.ch')
+ftp.login(user='Daten_ALVPH@blv-data-ingest.ch', passwd=FTP_PASS)
+
+# In das richtige Verzeichnis wechseln
+ftp.cwd("Moderhinke_2024_2025")
+
+# Datei herunterladen
+with open(local_path, "wb") as f:
+    ftp.retrbinary(f"RETR {filename}", f.write)
+
+ftp.quit()
